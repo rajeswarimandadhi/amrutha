a90d24cbb6b6ff80110ca2ab4f0b9f701eecd89f
rajeswarimandadhi <rajeswarimandadhi@gmail.com>
2022-10-14 11:18:10 +0530

    app

---
+++ WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        CustomerDBContext db = new CustomerDBContext();
+        [HttpGet]
+        public IEnumerable<TblCustomer> get()
+        {
+            return db.TblCustomers;
+        }
+    }
+}
